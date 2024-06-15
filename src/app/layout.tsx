db69d02eb4dee82d6018b2f19d1a102797fceaff
mindula-madhuhansa <mimmindula@gmail.com>
2024-06-16 00:33:58 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Quizz AI",
+  description:
+    "Quizz AI is a quiz app that uses AI to generate questions and answers.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={"dark"}>{children}</body>
+    </html>
+  );
+}
